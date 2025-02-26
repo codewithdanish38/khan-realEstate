040a6d5bb8b0bb9445fc523841a5d09685f99fda
Danish Khan <khanlko2002@gmail.com>
2025-02-26 10:30:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,9 @@ import Project from "./Component/Project";
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
+
       <Header/>
-=======
-        <Header /> 
->>>>>>> c0a5f60db0678c960238835b2ed98eb99add5bfa
+
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/signup" element={<SignUp />} />
